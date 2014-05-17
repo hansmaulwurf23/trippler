3603a56a0396f85220cc4537c571a3d4e253b4d4
Florian Löffler <loe.florian@gmail.com>
2014-05-18 01:04:06 +0200

    added some basic initialisation code to create a admin user and and roles to get a quick start

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,6 +5,24 @@ import de.tp.sec.UserRole
 class BootStrap {
 
     def init = { servletContext ->
+		
+			// create default roles
+			def adminRole = Role.findByAuthority('ROLE_ADMIN')?:new Role(authority: 'ROLE_ADMIN').save(flush: true)
+			def userRole = Role.findByAuthority('ROLE_USER')?:new Role(authority: 'ROLE_USER').save(flush: true)
+	   
+			// create default admin user
+			def adminUser = User.findByUsername('admin')
+			if (!adminUser) { 
+				log.warn("Creating default admin user - PLEASE CHANGE IMMEDIATELY AFTER FIRST LOGIN: username='admin' / password='trippler'")
+				adminUser = new User(username: 'admin', password:'trippler').save(flush: true)
+			}
+
+			if (!UserRole.findByUserAndRole(adminUser, adminRole))
+				new UserRole(user:adminUser, role:adminRole).save(flush:true)
+				
+			
+		
+			// check if everything went fine
 			assert User.count() >= 1 
 			assert Role.count() >= 2 
 			assert UserRole.count() >= 1
